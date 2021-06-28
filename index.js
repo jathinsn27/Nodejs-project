ec869a69545ae19890e8a3de34d5e83b281a0b63
jathinsn27 <71309726+jathinsn27@users.noreply.github.com>
2021-06-28 11:54:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 const express = require('express')
+const path = require('path')
+const router = express.Router()
+const dotenv = require('dotenv');
+const mysql = require('mysql')
 
 const app = express()
 
+dotenv.config({ path: './.env'});
+
+const db = mysql.createConnection({
+    
+    host       :  process.env.DATABASE_HOST,
+    user       :  process.env.DATABASE_USER,
+    password   :  process.env.DATABASE_PASSWORD,
+    database   :  process.env.DATABASE
+})
+
+db.connect((err) => {
+    if(err){
+        throw err
+    }
+    console.log('Database connected...')
+})
+
+const publicDirectory = path.join(__dirname, './public')
+app.use(express.static(publicDirectory))
+
+app.set('view engine', 'hbs')
 
 const student = require('./routes/student')
 const admin = require('./routes/admin')
-
-app.use(express.static('./public'))
+const auth = require('./routes/auth');
+const { dirname } = require('path');
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use('/', student)
 app.use('/admin', admin)
-
+app.use('/auth', auth)
 
 app.listen(5000, () => {
     console.log('server is listening on port 5000....')
