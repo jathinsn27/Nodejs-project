6e1d00501c8c10247773415404a7a7201146ef5a
jathinsn27 <71309726+jathinsn27@users.noreply.github.com>
2021-07-09 14:03:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 const express = require('express')
 const path = require('path')
 const router = express.Router()
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
 const mysql = require('mysql')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const hbs = require('hbs')
+const db = require('./database')
 
 const app = express()
 
 dotenv.config({ path: './.env'});
 
-const db = mysql.createConnection({
-    
-    host       :  process.env.DATABASE_HOST,
-    user       :  process.env.DATABASE_USER,
-    password   :  process.env.DATABASE_PASSWORD,
-    database   :  process.env.DATABASE
-})
-
-db.connect((err) => {
-    if(err){
-        throw err
-    }
-    console.log('Database connected...')
-})
-
-const publicDirectory = path.join(__dirname, './public')
-app.use(express.static(publicDirectory))
-
+const viewPath = path.join(__dirname, 'views')
+// app.use(express.static(publicDirectory))
 app.set('view engine', 'hbs')
+app.set('views', viewPath)
+app.use(cookieParser())
+app.use(express.static(__dirname + '/public'));
 
 const student = require('./routes/student')
 const admin = require('./routes/admin')
 const auth = require('./routes/auth');
+const autha = require('./routes/autha')
 const { dirname } = require('path');
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use('/', student)
 app.use('/admin', admin)
 app.use('/auth', auth)
+app.use('/autha', autha)
 
-app.listen(5000, () => {
-    console.log('server is listening on port 5000....')
+db.connect((err) => {
+    if(err){
+        console.error('Unable to connect to database.')
+        throw err
+    }
+    console.log('Database connected...')
+    app.listen(3000, () => {
+        console.log('server is listening on port 3000....')
+    })
 })
