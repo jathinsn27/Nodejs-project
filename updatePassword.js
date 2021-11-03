9a40a71063de7ec5eb6b2ac603443e4b6420c8b0
jathin sn <71309726+jathinsn27@users.noreply.github.com>
2021-11-04 00:04:59 +0530

    Update updatePassword.js

--- updatePassword.js
+++ updatePassword.js
@@ -1,23 +1,55 @@
-const forgotpassword2 = async (req, res) => {
-    const { password1, password2 } = req.body;
-    console.log(password1)
-    mail_token=student.mail_token
-    console.log(mail_token);
-    const decode = jwt.decode(mail_token)
+const forgotpassword =(req, res) => {
+    const { email } = req.body;
+    console.log(req.params.jwt);
+    const decode = jwt.decode(req.params.jwt);
     console.log(decode)
-    console.log(decode.user)
-    const id = decode.user
-    let hashedPassword = await bcrypt.hash(password1, PASSWORD_HASH_SECRET)
-    console.log(hashedPassword)
-    let sql = `UPDATE student_signup SET password='${hashedPassword}' WHERE sno='${id}'`
-    pool.query(sql, (error, results) => {
+    pool.query('SELECT email, sno FROM student_signup WHERE email = ?', [email],  (error, results) => {
         if(error){
-            console.error(error)  
+            console.log(error);
+        } else if( results.length === 0 ){
+            return res.render('signup', {
+                message: 'Email doesnot exist'
+            })
+        } else{
+            const id = results[0].sno;
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'testingdummybms@gmail.com',
+          pass: 'cookiethedog'
         }
-        else{
-            console.log(results)
-            res.status(200).redirect('../student')               
+      })
+      
+      try {
+          const emailToken = jwt.sign(
+            {
+              user: id,
+            },
+            process.env.JWT_SECRET,
+            {
+              expiresIn: '1d',
+            },
+            )
+            console.log(emailToken);
+            const url = `http://localhost:3000/student/forgot_password2/${emailToken}`
+
+      var mailOptions = {
+        from: 'testingdummybms@gmail.com',
+        to: email,  //email
+        subject: 'Password update mail',
+        html: `<h1>Welcome</h1> <a href="${url}">Click here to reset password</a>`
+      };
+      
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
         }
-    })
-    
+    }) 
+    }catch (err) {
+    console.error(err);
+    }
+}
+})
 }
